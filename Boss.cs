08350ab8ae445faec69520e3269e40116036aad7
zjs901 <zakjs901@gmail.com>
2025-04-13 14:23:22 +0100

    Added multiple rooms

---
+++ Boss.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace DungeonExplorer
+{
+    // Use of abstract class
+    public class Boss : Creature
+    {
+        private bool _isAlive;
+        public bool HasDroppedItem { get; set; }
+        private string _item;
+
+        public Boss(Room monsterRoom, string name, int health, int maxDamage, int minDamage, bool isAlive, string item) 
+            : base(monsterRoom, name, health, maxDamage, minDamage) 
+        {
+
+            IsAlive = isAlive;
+            _item = item;
+        }
+        // Use of override
+        public override void Attack(Creature target)
+        {
+            int monsterAttack = rand.Next(MinDamage, MaxDamage + 1);
+            target.Health -= monsterAttack;
+            Console.WriteLine($"{target.Name} hit back for {monsterAttack} damage.");
+
+        }
+
+        // Getters and setters, for the monsters, name, health, max damage, min damage, alive status and the item it drops
+        public bool IsAlive
+        {
+            get
+            {
+                return _isAlive;
+            }
+            set
+            {
+                _isAlive = value;
+            }
+        }
+
+        public string Item
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+    }
+}
